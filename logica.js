fc1ca99903354a4b511791037dc1a6f03651481d
José Romualdo <joseromualdo@outlook.com>
2021-04-19 20:29:06 -0300

    ajustando visualização do tabuleiro

--- logica.js
+++ logica.js
@@ -1,4 +1,80 @@
-function criarCasa(corCasa){
+function criarTabuleiro() {
+    const QTD_LINHAS = 8;
+
+    for (var linha = 1; linha <= QTD_LINHAS; linha++) {
+        criarLinhaComCasasAlternadas("#tabuleiro", linha);
+    }
+}
+
+function criarLinhaComCasasAlternadas(nomeDoContainer, indiceLinha) {
+    const QTD_COLUNAS = 8;
+
+    var linha = criarDivLinha();
+    var cabecalhoColuna = criarDivLinha();
+
+    for (var coluna = 1; coluna <= QTD_COLUNAS; coluna++) {
+
+        if (indiceLinha == 1) {
+            var celula = criarCabecalhoLinha(coluna)
+            cabecalhoColuna.append(celula);
+        }
+
+        var div = criarColuna(indiceLinha, coluna);
+
+        linha.append(div);
+    }
+
+    if (indiceLinha == 1) {
+        $(nomeDoContainer).append(cabecalhoColuna);
+    }
+    $(nomeDoContainer).append(linha);
+}
+
+function criarCabecalhoLinha(indiceLinha) {
+    var paragrafo = $("<p/>");
+    paragrafo.text(indiceLinha);
+
+    var innerDiv = $("<div/>");
+    innerDiv.addClass("vertical-center");
+    innerDiv.append(paragrafo);
+
+    var cabecalho = $("<div/>");
+    cabecalho.addClass("container-cabecalho");
+    cabecalho.addClass("float-left");
+    cabecalho.append(innerDiv);
+    return cabecalho;
+}
+
+function criarCabecalhoLinha(indiceColuna) {
+
+    var letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+    var paragrafo = $("<p/>");
+    paragrafo.text(letras[indiceColuna - 1]);
+
+    var cabecalho = $("<div/>");
+    cabecalho.addClass("casa");
+    cabecalho.addClass("float-left");
+    cabecalho.append(paragrafo);
+
+
+    return cabecalho;
+}
+
+
+
+function criarColuna(indiceLinha, indiceColuna) {
+    var corCasa = 'branca';
+
+    if (linhaIgualColuna(indiceLinha, indiceColuna)) {
+        corCasa = 'preta';
+    }
+
+    var div = criarCasa(corCasa);
+    return div;
+}
+
+function criarCasa(corCasa) {
     var div = $("<div/>");
     div.addClass("casa");
     div.addClass(corCasa);
@@ -7,33 +83,11 @@ function criarCasa(corCasa){
     return div;
 }
 
-function criarLinha(nomeDoContainer, linha) {
-    const QTD_COLUNAS = 8;
-
+function criarDivLinha() {
     var linha = $("<div/>");
     linha.addClass("linha");
 
-    for (var coluna = 1; coluna <= QTD_COLUNAS; coluna++) {
-        var corCasa = 'branca';
-
-        if (linhaIgualColuna(linha, coluna)) {
-            corCasa = 'preta';
-        }
-
-        var div = criarCasa(corCasa);
-
-        linha.append(div);        
-    }
-
-    $(nomeDoContainer).append(linha);
-}
-
-function criarTabuleiro(){
-    const QTD_LINHAS = 8;
-
-    for (var linha = 1; linha <= QTD_LINHAS; linha++) {
-        criarLinha("#tabuleiro", linha);
-    }
+    return linha;
 }
 
 function ePar(numero) {
